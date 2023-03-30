61e9bfeecea960990b70ab8b4b550a979b6c6acb
Jocelyn <jocelynvargas123@yahoo.com>
2023-03-30 00:12:37 -0700

    lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// code your solution here
+function superbowlWin(object){
+    let foundYear = undefined
+    if(foundYear = object.find(element => element.result === "W" )){
+        return foundYear.year
+    }
+}
+
